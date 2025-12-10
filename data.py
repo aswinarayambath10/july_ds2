12ad34a38b60230f37d16567df316f7b8f97797a
aswinarayambath10 <aswinarayambath10@gmail.com>
2025-12-10 14:29:15 +0530

    Refactor add function and add subtraction functionality
    Removed duplicate add function definitions and added a new sub function.

--- data.py
+++ data.py
@@ -1,60 +1,7 @@
 def add(x,y):
     return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
-def add(x,y):
-    return x + y
+
+
+
+def sub(num1, num2):
+    return num1 - num2
